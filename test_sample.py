5aeed81270f1cc93e359d882fe8629b5dc96fbfe
sowbagya <sowbagyasivakumar@gmail.com>
2026-04-08 18:16:46 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,2 +1,16 @@
+import pytest
+
 def test_basic():
     assert 1 == 1
+
+def test_math():
+    assert 2 + 2 == 4
+
+def test_string():
+    assert "devops".upper() == "DEVOPS"
+
+def test_list():
+    assert len([1,2,3]) == 3
+
+def test_true():
+    assert True
