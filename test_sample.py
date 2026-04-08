46a3718c484c9f5c3f4d177e48555921a3423e08
sowbagya <sowbagyasivakumar@gmail.com>
2026-04-08 18:35:30 +0530

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,16 +1,35 @@
-import pytest
+from utils import method_greedy, method_kmeans, decision_function
+import pandas as pd
 
-def test_basic():
-    assert 1 == 1
+def test_dataframe():
+    df = pd.DataFrame({
+        "Location ID": [1, 2],
+        "Distance from warehouse": [10, 20],
+        "Delivery Priority": ["High", "Low"]
+    })
+    assert not df.empty
+
+def test_greedy():
+    df = pd.DataFrame({
+        "Location ID": [1, 2, 3],
+        "Distance from warehouse": [10, 20, 30],
+        "Delivery Priority": ["High", "Medium", "Low"]
+    })
+    result = method_greedy(df)
+    assert len(result) == 5
+
+def test_kmeans():
+    df = pd.DataFrame({
+        "Location ID": [1, 2, 3],
+        "Distance from warehouse": [10, 20, 30],
+        "Delivery Priority": ["High", "Medium", "Low"]
+    })
+    result = method_kmeans(df)
+    assert len(result) == 5
+
+def test_decision():
+    res = decision_function(1, 2, 3, 4, 5, 6)
+    assert res in ["Greedy", "KMeans"]
 
 def test_math():
     assert 2 + 2 == 4
-
-def test_string():
-    assert "devops".upper() == "DEVOPS"
-
-def test_list():
-    assert len([1,2,3]) == 3
-
-def test_true():
-    assert True
